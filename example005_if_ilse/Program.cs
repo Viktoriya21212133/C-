eed9b0f9c8999ea9063e0d07d86b29294519df59
Viktoriya <shakhova_v@bk.ru>
2023-02-10 14:33:16 +0300

    Не работает программа с Tolower

---
+++ example005_if_ilse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Enter your name: ");
+string username = Console.ReadLine();
+
+if (username.Tolower == "masha")
+{
+    Console.WriteLine ("Yeah! It's Masha!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
