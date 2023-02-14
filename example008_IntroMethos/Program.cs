5a249da1f10d317428d3b9b21e2bd7fdc254be2a
Viktoriya <shakhova_v@bk.ru>
2023-02-14 15:49:41 +0300

    Стихийный метод нахождение максимального числа из 9+метод фуцнкции+решение в одну строку

--- example008_IntroMethos/Program.cs
+++ example008_IntroMethos/Program.cs
@@ -45,15 +45,17 @@ int b1 = 21;
 int c1 = 39;
 int a2 = 12;
 int b2 = 23;
-int c2 = 33;
+int c2 = 3325535;
 int a3 = 13;
 int b3 = 23;
 int c3 = 33;
 
 //Введем новую переменную макс, в которую положим значение первого аргумента
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max (max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max (max1, max2, max3);
+
+int max = Max (Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 
 Console.WriteLine ("Max = " +max);
