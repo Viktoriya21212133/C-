8ea6c009a94e775e7c2098ae9c7959c96fcf1664
Viktoriya <shakhova_v@bk.ru>
2023-02-14 16:34:13 +0300

    Задача на нахождение индекса массива по искомому числу

---
+++ example010_MethodArray/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Введем исходный массив
+int [] array = {1, 235, 531, 4, 6 , 15, 18, 235, 62};
+
+//введем размерность массива
+int n = 9;
+
+//введем число, которое будем искать
+int find = 235;
+
+//Введем нулевой индекс массива 
+int index = 0;
+
+// Далее пошел цикл, в котором идет проверка если индекс меньше n
+while (index < n)
+{
+ 
+ if (array [index] == find)
+  {
+    Console.WriteLine ("Index = "+index);
+    break;
+  }
+ index++;
+
+
+
+}
