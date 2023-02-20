699c600f2dcaa147a70969f0e790bdd5899f6772
Viktoriya <shakhova_v@bk.ru>
2023-02-20 14:07:30 +0300

    Hello,User

--- example002_HelloUser/Program.cs
+++ example002_HelloUser/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Inter your name");
+﻿// Программа Hello, User
+Console.Write("Inter your name");
 string username = Console.ReadLine ();
 Console. Write("Hello, ");
 Console.Write(username);
