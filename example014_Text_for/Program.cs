b9c74aa05de6e536131eb6360eda1d867e0fa961
Viktoriya <shakhova_v@bk.ru>
2023-02-24 22:03:00 +0300

    Дан текст. В тексте нужно все пробелы заменить черточками, мателькие буквы к заменить большими к, а большие С заменить маленькими с

---
+++ example014_Text_for/Program.cs
@@ -0,0 +1,28 @@
+﻿// Дан текст. В тексте нужно все пробелы заменить черточками, мателькие буквы "к" заменить большими "К", а большие "С" заменить маленькими "с".
+string text = " - Я думаю, - сказал князь, улыбаясь, - что, "
++ "ежели бы вас послали вместо нашего милого Винцегероде, "
++ "вы бы взяли приступом согласие прусского короля. "
++" Вы так красноречивы. Вы дадите мне чаю? ";
+
+// string s = "qwerty"
+//             012345
+// s [3] // r
+
+string Replase (string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int lenght = text.Length;
+    for (int i = 0; i <lenght; i++)
+    {
+        if (text[i] == oldValue) 
+        result = result + $"{newValue}";
+        else 
+        result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replase (text, ' ', '|');
+Console.WriteLine (newText);
+Console.WriteLine ();
+newText = Replase (newText, 'к', 'К');
+Console.WriteLine (newText);
